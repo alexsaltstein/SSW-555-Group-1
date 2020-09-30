2e95c080934481d2ad7c0de5bccc0a7a85c33ac3
Brandon Patton <bpatton@stevens.edu>
2020-09-30 15:31:47 -0400

    Added US09 and US10

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 #This is the utilities script for how we are going to implement stories for each sprint
 from datetime import datetime
 from datetime import date
+import dateutil.relativedelta
 
 '''
     This function loops through individuals and makes sure that
@@ -25,7 +26,7 @@ def us01DatesBeforeCurrentDate(individuals, families):
         if (now - datetime.strptime(x.death, '%d %b %Y')).days <= 0:
             print(KEY_WORD + x.iD + ": Died " + x.death + " after today " + now)
             output.append(x.iD)
-    for y in familes:
+    for y in families:
         if (now - datetime.strptime(y.married, '%d %b %Y')).days <= 0:
             print(KEY_WORD + x.iD + ": Married " + y.married + " after today " + now)
             output.append(y.iD)
@@ -116,4 +117,71 @@ def us08BirthBeforeMarriage(families, individuals):
                 if isBirthBeforeMarriage:
                     print(KEY_WORD + fam.iD + ": Married " + fam.married + " after birth of " + indi.name + " on " + indi.birthday)
                     output.append(fam.iD)
+    return output
+
+def findParentDeath(indi_iD, listOfIndis):
+    '''Loops through list of individuals, finds a specific parent's death date, and returns that information'''
+    for i in listOfIndis:
+        if indi_iD == i.iD:
+            if not i.alive:
+                return i.death
+
+def findChildBday(child_iD, listOfIndis):
+    '''Loops through list of individuals, finds a specific individuals birth date, and returns that information'''
+    for i in listOfIndis:
+        if child_iD == i.iD:
+            return i.birthday
+
+def us09BirthBeforeDeathOfParents(listOfFamilies, listOfIndividuals):
+    '''Families have a list of children. The list contains the individual IDs for each child
+        For example: Family ID: F1 has Children with IDs: ['I1', 'I4', 'I5']
+
+        This function loops through the list of families provided through the function call and stores
+        the list of children ids, the husband id, and the wife id of one family at each iteration.
+        On the same iteration, also finds the death dates of each parent if applicable with the use
+        of a helper function called "findParentDeath". After getting this information, on the same
+        iteration the function then loops through the now collected list of child ids and finds one child's
+        birthday per iteration.  The birthday stored is then converted to datetime object format.
+        The program then checks if the husband's or wife's death date indeed exists and if so converts
+        this death date to datetime object format, and then does the
+        appropriate comparison between the stored child birthdate and the specific parent death date.
+        If the mother's death date predates the child's birthday, this is an anomaly and the program prints
+        an according error message and collects the problematic child id in a list called "output".  If the father's
+        death date predates 9 months before the child was born, this is an anomaly and the program prints
+        an according error message and collects the problematic child id in a list called "output".  Once the function
+        is finished looping through the provided list of families, it returns the output list of the problematic child ids
+        collected throughout the iterations.'''
+    output = []
+    KEY_WORD = "ERROR: INDIVIDUALS: US09: "
+    for f in listOfFamilies:
+        listOfChildren = f.children     #List of strings of individual ids of children
+        husband = f.husbId              #string of husband's individual id
+        wife = f.wifeId                 #string of wife's individual id
+        husbandDeath = findParentDeath(husband, listOfIndividuals)     #store husband's death date if applicable
+        wifeDeath = findParentDeath(wife, listOfIndividuals)           #store wife's death date if applicable
+        for c in listOfChildren:                                       #loop through list of children IDs
+            childBirthday = findChildBday(c, listOfIndividuals)        #get child's birthday
+            cBDatetime = datetime.strptime(childBirthday, "%d %b %Y")
+            if wifeDeath is not None:                                   #check if wifeDeath exists
+                wDDatetime = datetime.strptime(wifeDeath, "%d %b %Y")   #convert wife's death to datetime object from string
+                if cBDatetime > wDDatetime:                             #if Mother's death date is before birth of child, not possible
+                    print(KEY_WORD + "ANOMALY: Mother's death cannot come before Child's birth")
+                    output.append(c)        #appends problematic child id to output
+            if husbandDeath is not None:                                                           #check if husbandDeath exists
+                hDDatetime = datetime.strptime(husbandDeath, "%d %b %Y")                           #convert husband's death to datetime object from string
+                if (cBDatetime - dateutil.relativedelta.relativedelta(months=9)) > hDDatetime:     #left operand is conception date. if that's greater than the father's death date, that is impossible
+                    print(KEY_WORD + "ANOMALY: Father's death cannot come after 9 months before Child's birth (Child's conception date)")
+                    output.append(c)        #appends problematic child id to output
+    return output
+
+def us10MarriageAfter14(listOfFamilies, listOfIndividuals):
+    output = []
+    KEY_WORD = "ERROR: INDIVIDUALS: US10: "
+    for f in listOfFamilies:
+        husband = f.husbId
+        wife = f.wifeId
+        for i in listOfIndividuals:
+            if (i.iD == husband or i.iD == wife) and (datetime.strptime(f.married, '%d %b %Y') - datetime.strptime(i.birthday, '%d %b %Y')).days/365 < 14:
+                print(KEY_WORD + i.iD + " got married under the age of 14.")
+                output.append(i.iD)
     return output
